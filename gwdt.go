5742b3b733ff6f3df2dc0b19daceb10315ff9457
3203645230 <3203645230@qq.com>
2024-12-16 17:23:52 +0800

    增加注释

--- gwdt.go
+++ gwdt.go
@@ -13,6 +13,83 @@ import (
 	"time"
 )
 
+// gwdt旺店通直连客户端
+
+// NewGwdtClient 旺店通直连客户端构建函数
+func NewGwdtClient(config Config) *Client {
+	return &Client{Config: config}
+}
+
+// Config 旺店通直连配置
+type Config struct {
+	Url       string `json:"url"` // 接口请求地址
+	V         string `json:"v"`
+	Sid       string `json:"sid"`       // 卖家账号
+	AppKey    string `json:"appkey"`    // 旺店通旗舰版appkey
+	AppSecret string `json:"appsecret"` // 旺店通旗舰版appsecret
+}
+
+// getSecret 获取旺店通密钥
+func (c *Config) getSecret() (secret string, salt string, error error) {
+	parts := strings.Split(c.AppSecret, ":")
+	if len(parts) != 2 {
+		return "", "", errors.New("invalid appsecret format")
+	}
+	secret = parts[0]
+	salt = parts[1]
+	return secret, salt, nil
+}
+
+// Pager 分页器
+type Pager struct {
+	PageSize  int  // 每页数量
+	PageNo    int  // 页码
+	CalcTotal bool // 是否计算总条数
+}
+
+// Request 请求参数
+type Request struct {
+	Method string
+	Params map[string]interface{}
+	Pager  *Pager
+}
+
+// WdtError 旺店通错误
+type WdtError struct {
+	Message      string `json:"Message"`       // 接口错误信息
+	RequestError error  `json:"request_error"` // 请求错误
+}
+
+// Response 旺店通直连响应
+type Response struct {
+	Request    *Request  // 原始请求
+	Status     int64     // 返回状态码，-1为请求失败，其他为接口返回状态码
+	Error      *WdtError // 错误信息
+	Timestamp  int64     // 按照旺店通规则的请求时间戳
+	Sign       string    // 按照旺店通规则的签名
+	Data       string    // 原始返回数据json字符串
+	TotalCount int64     // 总条数，仅计算分页时使用
+}
+
+// GetByte 获取原始返回数据
+func (c *Response) GetByte() []byte {
+	return []byte(c.Data)
+}
+
+// Get 按键名获取原始返回数据
+func (c *Response) Get(key string) string {
+	return gjson.Get(c.Data, key).String()
+}
+
+// HasMore 判断是否还有更多数据，仅计算分页时使用
+func (c *Response) HasMore() bool {
+	if c.Request.Pager == nil || !c.Request.Pager.CalcTotal {
+		return false
+	}
+	return c.TotalCount > int64((c.Request.Pager.PageNo+1)*c.Request.Pager.PageSize)
+}
+
+// Context 旺店通直连上下文管理器
 type Context struct {
 	Request     *Request
 	Response    *Response
@@ -21,19 +98,27 @@ type Context struct {
 	no          int
 }
 
+// Next 上下文跳转
 func (c *Context) Next() {
 	c.no += 1
 	nextFunc := *c.middlewares[c.no]
 	nextFunc(c)
 }
 
+// Client 旺店通直连客户端
 type Client struct {
 	Config      Config
 	middlewares []*func(*Context)
 }
 
+// Use 使用旺店通中间件
+func (c *Client) Use(middleware func(ctx *Context)) {
+	c.middlewares = append(c.middlewares, &middleware)
+}
+
+// getSign 获取旺店通请求签名
 func (c *Client) getSign(timestamp int64, dataWrapper []byte, pager *Pager, method string) (string, map[string]string, error) {
-	secret, salt, err := c.Config.GetSecret()
+	secret, salt, err := c.Config.getSecret()
 	if err != nil {
 		return "", nil, err
 	}
@@ -72,6 +157,7 @@ func (c *Client) getSign(timestamp int64, dataWrapper []byte, pager *Pager, meth
 	return sign, params, nil
 }
 
+// Call 执行请求方法，含中间件
 func (c *Client) Call(request *Request) *Response {
 	rq := c.rq
 	tryMiddlewares := append(c.middlewares, &rq)
@@ -86,6 +172,17 @@ func (c *Client) Call(request *Request) *Response {
 	nextFunc(&ctx)
 	return ctx.Response
 }
+
+// CallWithoutMiddleware 执行请求方法，不包含中间件
+func (c *Client) CallWithoutMiddleware(request *Request) *Response {
+	context := Context{
+		Request: request,
+	}
+	c.rq(&context)
+	return context.Response
+}
+
+// rq 请求底层方法
 func (c *Client) rq(ctx *Context) {
 	request := ctx.Request
 	res := Response{
@@ -147,88 +244,27 @@ func (c *Client) rq(ctx *Context) {
 	ctx.Response = &res
 	return
 }
-func (c *Client) CallWithoutMiddleware(request *Request) *Response {
-	context := Context{
-		Request: request,
-	}
-	c.rq(&context)
-	return context.Response
-}
 
-type Config struct {
-	Url       string `json:"url"` // 接口请求地址
-	V         string `json:"v"`
-	Sid       string `json:"sid"`       // 卖家账号
-	AppKey    string `json:"appkey"`    // 旺店通旗舰版appkey
-	AppSecret string `json:"appsecret"` // 旺店通旗舰版appsecret
-}
+// 奇门客户端
 
-func (c *Config) GetSecret() (secret string, salt string, error error) {
-	parts := strings.Split(c.AppSecret, ":")
-	if len(parts) != 2 {
-		return "", "", errors.New("invalid appsecret format")
-	}
-	secret = parts[0]
-	salt = parts[1]
-	return secret, salt, nil
-}
-
-type Pager struct {
-	PageSize  int  // 每页数量
-	PageNo    int  // 页码
-	CalcTotal bool // 是否计算总条数
-}
-
-type Request struct {
-	Method string
-	Params map[string]interface{}
-	Pager  *Pager
-}
-type WdtError struct {
-	Message      string `json:"Message"`
-	RequestError error  `json:"request_error"`
-}
-type Response struct {
-	Request    *Request
-	Status     int64
-	Error      *WdtError
-	Timestamp  int64  // 按照旺店通规则的请求时间戳
-	Sign       string // 按照旺店通规则的签名
-	Data       string
-	TotalCount int64
-}
-
-func (c *Response) GetByte() []byte {
-	return []byte(c.Data)
-}
-func (c *Response) Get(key string) string {
-	return gjson.Get(c.Data, key).String()
-}
-func (c *Response) HasMore() bool {
-	if c.Request.Pager == nil || !c.Request.Pager.CalcTotal {
-		return false
-	}
-	return c.TotalCount > int64((c.Request.Pager.PageNo+1)*c.Request.Pager.PageSize)
-}
-
-func NewGwdtClient(config Config) *Client {
-	return &Client{Config: config}
-}
+// NewGwdtQimenClient 奇门客户端构建函数
 func NewGwdtQimenClient(qimenConfig QimenConfig) *QimenClient {
 	return &QimenClient{Config: qimenConfig}
 }
 
+// QimenConfig 奇门配置
 type QimenConfig struct {
-	QimenUrl       string `json:"qimen_url"`
-	QimenAppKey    string `json:"qimen_appkey"`
-	QimenAppSecret string `json:"qimen_appsecret"`
-	Sid            string `json:"sid"`           // 卖家账号
-	WdtAppKey      string `json:"wdt_appkey"`    // 旺店通旗舰版appkey
-	WdtAppSecret   string `json:"wdt_appsecret"` // 旺店通旗舰版appsecret
-	TargetAppkey   string `json:"target_appkey"`
+	QimenUrl       string `json:"qimen_url"`       // 奇门地址
+	QimenAppKey    string `json:"qimen_appkey"`    // 奇门appkey
+	QimenAppSecret string `json:"qimen_appsecret"` // 奇门appsecret
+	Sid            string `json:"sid"`             // 卖家账号
+	WdtAppKey      string `json:"wdt_appkey"`      // 旺店通旗舰版appkey
+	WdtAppSecret   string `json:"wdt_appsecret"`   // 旺店通旗舰版appsecret
+	TargetAppkey   string `json:"target_appkey"`   // 目标appkey
 }
 
-func (c *QimenConfig) GetSecret() (secret string, salt string, error error) {
+// getSecret 获取密钥与盐
+func (c *QimenConfig) getSecret() (secret string, salt string, error error) {
 	parts := strings.Split(c.WdtAppSecret, ":")
 	if len(parts) != 2 {
 		return "", "", errors.New("invalid appsecret format")
@@ -238,26 +274,56 @@ func (c *QimenConfig) GetSecret() (secret string, salt string, error error) {
 	return secret, salt, nil
 }
 
-type QimenContext struct {
-	Request     *QimenRequest
-	Response    *QimenResponse
-	Client      *QimenClient
-	middlewares []*func(ctx *QimenContext)
-	no          int
-}
-
+// QimenClient 奇门客户端
 type QimenClient struct {
 	Config      QimenConfig
 	middlewares []*func(*QimenContext)
 }
 
-func (c *QimenClient) Use(middleware func(ctx *QimenContext)) {
-	c.middlewares = append(c.middlewares, &middleware)
-}
-
+// QimenRequest 奇门请求
 type QimenRequest = Request
 
-func (c *QimenRequest) GetSortedParams() ([]byte, error) {
+// QimenResponse 奇门响应
+type QimenResponse struct {
+	Request    *QimenRequest // 原始请求
+	Status     int64         // 状态码，-1为请求失败，0为请求成功，1为返回错误
+	Error      *QimenError   // 返回错误
+	DateTime   string        // 按照旺店通规则的请求时间戳
+	Sign       string        // 奇门签名
+	WdtSign    string        // 按照旺店通规则的签名
+	Data       string        // 返回数据json字符串
+	TotalCount int64         // 分页查询返回的总记录数，仅当分页获取总数时返回
+}
+type QimenError struct {
+	Flag         string // 奇门返回的错误标识
+	RequestId    string // 奇门返回的请求id
+	Code         string // 奇门返回的错误码
+	Message      string // 奇门返回的错误信息
+	SubCode      string // 旺店通错误码
+	SubMsg       string // 旺店通错误信息
+	RequestError error  // 原始请求错误
+}
+
+// GetByte 获取奇门返回数据
+func (c *QimenResponse) GetByte() []byte {
+	return []byte(c.Data)
+}
+
+// Get 按键获取奇门返回数据
+func (c *QimenResponse) Get(key string) string {
+	return gjson.Get(c.Data, key).String()
+}
+
+// HasMore 是否还有更多数据，仅分页且获取总数时返回
+func (c *QimenResponse) HasMore() bool {
+	if c.Request.Pager == nil || !c.Request.Pager.CalcTotal {
+		return false
+	}
+	return c.TotalCount > int64((c.Request.Pager.PageNo)*c.Request.Pager.PageSize)
+}
+
+// getSortedParams 获取排序后的请求参数
+func (c *QimenRequest) getSortedParams() ([]byte, error) {
 	// 提取所有键
 	keys := make([]string, 0, len(c.Params))
 	for k := range c.Params {
@@ -278,42 +344,23 @@ func (c *QimenRequest) GetSortedParams() ([]byte, error) {
 	return jsonData, nil
 }
 
-type QimenError struct {
-	Flag         string
-	RequestId    string
-	Code         string
-	Message      string
-	SubCode      string
-	SubMsg       string
-	RequestError error
+// QimenContext 奇门上下文管理器
+type QimenContext struct {
+	Request     *QimenRequest
+	Response    *QimenResponse
+	Client      *QimenClient
+	middlewares []*func(ctx *QimenContext)
+	no          int
 }
 
-type QimenResponse struct {
-	Request    *QimenRequest
-	Status     int64
-	Error      *QimenError
-	DateTime   string // 按照旺店通规则的请求时间戳
-	Sign       string
-	WdtSign    string // 按照旺店通规则的签名
-	Data       string
-	TotalCount int64
+// Use 添加奇门中间件
+func (c *QimenClient) Use(middleware func(ctx *QimenContext)) {
+	c.middlewares = append(c.middlewares, &middleware)
 }
 
-func (c *QimenResponse) GetByte() []byte {
-	return []byte(c.Data)
-}
-func (c *QimenResponse) Get(key string) string {
-	return gjson.Get(c.Data, key).String()
-}
-func (c *QimenResponse) HasMore() bool {
-	if c.Request.Pager == nil || !c.Request.Pager.CalcTotal {
-		return false
-	}
-	return c.TotalCount > int64((c.Request.Pager.PageNo)*c.Request.Pager.PageSize)
-}
-
+// getSign 获取奇门签名
 func (c *QimenClient) getSign(timestamp string, dataWrapper []byte, pager *Pager, method string) (string, string, map[string]string, error) {
-	_, wdtSalt, err := c.Config.GetSecret()
+	_, wdtSalt, err := c.Config.getSecret()
 	if err != nil {
 		return "", "", nil, err
 	}
@@ -360,8 +407,9 @@ func (c *QimenClient) getSign(timestamp string, dataWrapper []byte, pager *Pager
 	return sign, wdtSign, params, nil
 }
 
+// getWdtSign 获取旺店通签名
 func (c *QimenClient) getWdtSign(datetime string, dataWrapper []byte, pager *Pager, method string) (string, error) {
-	wdtSecret, wdtSalt, err := c.Config.GetSecret()
+	wdtSecret, wdtSalt, err := c.Config.getSecret()
 	if err != nil {
 		return "", err
 	}
@@ -394,9 +442,8 @@ func (c *QimenClient) getWdtSign(datetime string, dataWrapper []byte, pager *Pag
 	connString += wdtSecret
 	return gwdtUtils.MD5(connString), nil
 }
-func (c *Client) Use(middleware func(ctx *Context)) {
-	c.middlewares = append(c.middlewares, &middleware)
-}
+
+// Call 执行奇门接口请求
 func (c *QimenClient) Call(request *QimenRequest) *QimenResponse {
 	rq := c.rq
 	tryMiddlewares := append(c.middlewares, &rq)
@@ -412,6 +459,7 @@ func (c *QimenClient) Call(request *QimenRequest) *QimenResponse {
 	return ctx.Response
 }
 
+// CallWithoutMiddleware 执行奇门接口请求，不执行中间件
 func (c *QimenClient) CallWithoutMiddleware(request *QimenRequest) *QimenResponse {
 	rq := c.rq
 	ctx := QimenContext{
@@ -421,6 +469,8 @@ func (c *QimenClient) CallWithoutMiddleware(request *QimenRequest) *QimenRespons
 	rq(&ctx)
 	return ctx.Response
 }
+
+// rq 奇门底层请求方法
 func (c *QimenClient) rq(ctx *QimenContext) {
 	request := ctx.Request
 	res := QimenResponse{
@@ -433,7 +483,7 @@ func (c *QimenClient) rq(ctx *QimenContext) {
 	if request.Params == nil {
 		dataWrapper = []byte("{}")
 	} else {
-		dataWrapper, err = request.GetSortedParams()
+		dataWrapper, err = request.getSortedParams()
 		if err != nil {
 			res.Error = &QimenError{
 				Message:      "Failed to marshal params",
