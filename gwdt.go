45229b06b2a0ba05558c1ef5d3a30c2bc886d8b2
bernardsimon <bernardziyi@gmail.com>
2025-06-20 15:41:47 +0800

    fix 项目结构

--- gwdt.go
+++ gwdt.go
@@ -1,44 +1,38 @@
 package gwdt
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/BernardSimon/gwdt/gwdtUtils"
-	"github.com/levigross/grequests"
-	"github.com/tidwall/gjson"
-	"reflect"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"sort"
 	"strconv"
 	"strings"
 	"time"
 )
 
-// gwdt旺店通直连客户端
-
-// NewGwdtClient 旺店通直连客户端构建函数
-func NewGwdtClient(config Config) *Client {
-	return &Client{Config: config}
+// md5Sum 计算字符串的MD5值
+func md5Sum(s string) string {
+	hash := md5.New()
+	hash.Write([]byte(s))
+	return hex.EncodeToString(hash.Sum(nil))
 }
 
-// Config 旺店通直连配置
-type Config struct {
-	Url       string `json:"url"` // 接口请求地址
-	V         string `json:"v"`
-	Sid       string `json:"sid"`       // 卖家账号
-	AppKey    string `json:"appkey"`    // 旺店通旗舰版appkey
-	AppSecret string `json:"appsecret"` // 旺店通旗舰版appsecret
+// GwdtError 旺店通错误
+type WdtError struct {
+	Message      string `json:"message"`       // 接口错误信息
+	RequestError error  `json:"request_error"` // 请求错误
 }
 
-// getSecret 获取旺店通密钥
-func (c *Config) getSecret() (secret string, salt string, error error) {
-	parts := strings.Split(c.AppSecret, ":")
-	if len(parts) != 2 {
-		return "", "", errors.New("invalid appsecret format")
+func (e *WdtError) Error() string {
+	if e.RequestError != nil {
+		return fmt.Sprintf("WdtError: %s, RequestError: %v", e.Message, e.RequestError)
 	}
-	secret = parts[0]
-	salt = parts[1]
-	return secret, salt, nil
+	return fmt.Sprintf("WdtError: %s", e.Message)
 }
 
 // Pager 分页器
@@ -53,12 +47,6 @@ type Request struct {
 	Method string
 	Params interface{}
 	Pager  *Pager
-}
-
-// WdtError 旺店通错误
-type WdtError struct {
-	Message      string `json:"Message"`       // 接口错误信息
-	RequestError error  `json:"request_error"` // 请求错误
 }
 
 // Response 旺店通直连响应
@@ -79,7 +67,24 @@ func (c *Response) GetByte() []byte {
 
 // Get 按键名获取原始返回数据
 func (c *Response) Get(key string) string {
-	return gjson.Get(c.Data, key).String()
+	var dataMap map[string]interface{}
+	err := json.Unmarshal([]byte(c.Data), &dataMap)
+	if err != nil {
+		return ""
+	}
+
+	// Simple JSON path parsing for basic cases, can be extended for complex paths
+	keys := strings.Split(key, ".")
+	var current interface{} = dataMap
+	for _, k := range keys {
+		if m, ok := current.(map[string]interface{}); ok {
+			current = m[k]
+		} else {
+			return ""
+		}
+	}
+
+	return fmt.Sprintf("%v", current)
 }
 
 // HasMore 判断是否还有更多数据，仅计算分页时使用
@@ -102,14 +107,43 @@ type Context struct {
 // Next 上下文跳转
 func (c *Context) Next() {
 	c.no += 1
-	nextFunc := *c.middlewares[c.no]
-	nextFunc(c)
+	if c.no < len(c.middlewares) {
+		nextFunc := *c.middlewares[c.no]
+		nextFunc(c)
+	}
+}
+
+// Config 旺店通直连配置
+type Config struct {
+	Url       string `json:"url"` // 接口请求地址
+	V         string `json:"v"`
+	Sid       string `json:"sid"`       // 卖家账号
+	AppKey    string `json:"appkey"`    // 旺店通旗舰版appkey
+	AppSecret string `json:"appsecret"` // 旺店通旗舰版appsecret
+}
+
+// getSecret 获取旺店通密钥
+func (c *Config) getSecret() (secret string, salt string, error error) {
+	parts := strings.Split(c.AppSecret, ":")
+	if len(parts) != 2 {
+		return "", "", errors.New("invalid appsecret format")
+	}
+	secret = parts[0]
+	salt = parts[1]
+	return secret, salt, nil
 }
 
 // Client 旺店通直连客户端
 type Client struct {
 	Config      Config
 	middlewares []*func(*Context)
+}
+
+// NewGwdtClient 旺店通直连客户端构建函数
+func NewGwdtClient(config Config) *Client {
+	c := &Client{Config: config}
+	c.Use(c.rq)
+	return c
 }
 
 // Use 使用旺店通中间件
@@ -152,7 +186,7 @@ func (c *Client) getSign(timestamp int64, dataWrapper []byte, pager *Pager, meth
 		signConn += k + value
 	}
 	signConn += secret
-	sign := gwdtUtils.MD5(signConn)
+	sign := md5Sum(signConn)
 	params["sign"] = sign
 	delete(params, "body")
 	return sign, params, nil
@@ -160,27 +194,27 @@ func (c *Client) getSign(timestamp int64, dataWrapper []byte, pager *Pager, meth
 
 // Call 执行请求方法，含中间件
 func (c *Client) Call(request *Request) *Response {
-	rq := c.rq
-	tryMiddlewares := append(c.middlewares, &rq)
 	ctx := Context{
 		Request:     request,
 		Response:    nil,
 		Client:      c,
-		middlewares: tryMiddlewares,
+		middlewares: c.middlewares,
 		no:          0,
 	}
-	nextFunc := *tryMiddlewares[0]
-	nextFunc(&ctx)
+	if len(c.middlewares) > 0 {
+		nextFunc := *c.middlewares[0]
+		nextFunc(&ctx)
+	}
 	return ctx.Response
 }
 
 // CallWithoutMiddleware 执行请求方法，不包含中间件
 func (c *Client) CallWithoutMiddleware(request *Request) *Response {
-	context := Context{
+	ctx := Context{
 		Request: request,
 	}
-	c.rq(&context)
-	return context.Response
+	c.rq(&ctx)
+	return ctx.Response
 }
 
 // rq 请求底层方法
@@ -191,46 +225,28 @@ func (c *Client) rq(ctx *Context) {
 		Status:    -1,
 		Timestamp: time.Now().Unix() - 1325347200,
 	}
-	dataWrapper := []byte("")
-	if request.Params == nil {
-		dataWrapper = []byte("[{}]")
-	} else if reflect.TypeOf(request.Params) == reflect.TypeOf(map[string]interface{}{}) {
+	dataWrapper := []byte("[{}]")
+	if request.Params != nil {
 		var err error
-		dataWrapper, err = json.Marshal([]interface{}{request.Params})
+		// Marshal params to JSON first
+		tempData, err := json.Marshal(request.Params)
 		if err != nil {
 			res.Error = &WdtError{
-				RequestError: err,
+				RequestError: fmt.Errorf("failed to marshal request params: %w", err),
 			}
 			ctx.Response = &res
 			return
 		}
-	} else if reflect.TypeOf(request.Params) == reflect.TypeOf([]interface{}{}) {
-		var err error
-		dataWrapper, err = json.Marshal(request.Params)
-		if err != nil {
-			res.Error = &WdtError{
-				RequestError: err,
-			}
-			ctx.Response = &res
-			return
+
+		// Check if the marshaled JSON is an array
+		if len(tempData) > 0 && tempData[0] == '[' {
+			dataWrapper = tempData
+		} else {
+			// If it's an object, wrap it in an array
+			dataWrapper = []byte(fmt.Sprintf("[%s]", tempData))
 		}
-	} else if reflect.TypeOf(request.Params) == reflect.TypeOf([]map[string]interface{}{}) {
-		var err error
-		dataWrapper, err = json.Marshal(request.Params)
-		if err != nil {
-			res.Error = &WdtError{
-				RequestError: err,
-			}
-			ctx.Response = &res
-			return
-		}
-	} else {
-		res.Error = &WdtError{
-			RequestError: errors.New("invalid params type"),
-		}
-		ctx.Response = &res
-		return
 	}
+
 	var err error
 	var params map[string]string
 	res.Sign, params, err = c.getSign(res.Timestamp, dataWrapper, request.Pager, request.Method)
@@ -241,47 +257,100 @@ func (c *Client) rq(ctx *Context) {
 		ctx.Response = &res
 		return
 	}
-	resp, err := grequests.Post(c.Config.Url, &grequests.RequestOptions{JSON: dataWrapper, Params: params})
+
+	// Build HTTP request
+	httpReq, err := http.NewRequest("POST", c.Config.Url, strings.NewReader(string(dataWrapper)))
 	if err != nil {
 		res.Error = &WdtError{
 			RequestError: err,
 		}
 		ctx.Response = &res
 		return
-	} else if resp == nil {
+	}
+	httpReq.Header.Set("Content-Type", "application/json")
+
+	q := httpReq.URL.Query()
+	for k, v := range params {
+		q.Add(k, v)
+	}
+	httpReq.URL.RawQuery = q.Encode()
+
+	client := &http.Client{Timeout: 30 * time.Second}
+	httpResp, err := client.Do(httpReq)
+	if err != nil {
 		res.Error = &WdtError{
-			RequestError: errors.New("request failed"),
+			RequestError: err,
 		}
 		ctx.Response = &res
 		return
-	} else {
-		status := gjson.Get(resp.String(), "status")
-		if status.Int() != 0 {
-			res.Status = status.Int()
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(httpResp.Body)
+
+	body, err := ioutil.ReadAll(httpResp.Body)
+	if err != nil {
+		res.Error = &WdtError{
+			RequestError: err,
+		}
+		ctx.Response = &res
+		return
+	}
+
+	var rawResponse map[string]interface{}
+	err = json.Unmarshal(body, &rawResponse)
+	if err != nil {
+		res.Error = &WdtError{
+			RequestError: err,
+		}
+		ctx.Response = &res
+		return
+	}
+
+	statusVal, ok := rawResponse["status"].(float64)
+	if !ok || int64(statusVal) != 0 {
+		res.Status = -1 // Default to -1 if status is not 0 or not found
+		if ok {
+			res.Status = int64(statusVal)
+		}
+		messageVal, _ := rawResponse["message"].(string)
+		res.Error = &WdtError{
+			Message: messageVal,
+		}
+		ctx.Response = &res
+		return
+	}
+
+	res.Status = 0
+	dataVal, ok := rawResponse["data"].(interface{})
+	if ok {
+		dataBytes, _ := json.Marshal(dataVal)
+		res.Data = string(dataBytes)
+	}
+
+	if request.Pager != nil && request.Pager.CalcTotal {
+		var dataMap map[string]interface{}
+		err = json.Unmarshal([]byte(res.Data), &dataMap)
+		if err != nil {
 			res.Error = &WdtError{
-				Message: gjson.Get(resp.String(), "message").String(),
+				RequestError: err,
 			}
 			ctx.Response = &res
 			return
 		}
-		res.Status = 0
-		res.Data = gjson.Get(resp.String(), "data").String()
-		if request.Pager != nil && request.Pager.CalcTotal {
-			res.TotalCount = gjson.Get(res.Data, "total_count").Int()
+		if totalCountVal, ok := dataMap["total_count"].(float64); ok {
+			res.TotalCount = int64(totalCountVal)
 		} else {
 			res.TotalCount = 0
 		}
+	} else {
+		res.TotalCount = 0
 	}
 	ctx.Response = &res
 	return
 }
 
 // 奇门客户端
-
-// NewGwdtQimenClient 奇门客户端构建函数
-func NewGwdtQimenClient(qimenConfig QimenConfig) *QimenClient {
-	return &QimenClient{Config: qimenConfig}
-}
 
 // QimenConfig 奇门配置
 type QimenConfig struct {
@@ -311,6 +380,13 @@ type QimenClient struct {
 	middlewares []*func(*QimenContext)
 }
 
+// NewGwdtQimenClient 奇门客户端构建函数
+func NewGwdtQimenClient(qimenConfig QimenConfig) *QimenClient {
+	c := &QimenClient{Config: qimenConfig}
+	c.Use(c.rq)
+	return c
+}
+
 // QimenRequest 奇门请求
 type QimenRequest = Request
 
@@ -330,9 +406,18 @@ type QimenError struct {
 	RequestId    string // 奇门返回的请求id
 	Code         string // 奇门返回的错误码
 	Message      string // 奇门返回的错误信息
-	SubCode      string // 旺店通错误码
-	SubMsg       string // 旺店通错误信息
+	SubCode      string // 奇门返回的子错误码
+	SubMsg       string // 奇门返回的子错误信息
 	RequestError error  // 原始请求错误
+}
+
+func (e *QimenError) Error() string {
+	if e.RequestError != nil {
+		return fmt.Sprintf("QimenError: Flag=%s, RequestId=%s, Code=%s, Message=%s, SubCode=%s, SubMsg=%s, RequestError: %v",
+			e.Flag, e.RequestId, e.Code, e.Message, e.SubCode, e.SubMsg, e.RequestError)
+	}
+	return fmt.Sprintf("QimenError: Flag=%s, RequestId=%s, Code=%s, Message=%s, SubCode=%s, SubMsg=%s",
+		e.Flag, e.RequestId, e.Code, e.Message, e.SubCode, e.SubMsg)
 }
 
 // GetByte 获取奇门返回数据
@@ -342,7 +427,24 @@ func (c *QimenResponse) GetByte() []byte {
 
 // Get 按键获取奇门返回数据
 func (c *QimenResponse) Get(key string) string {
-	return gjson.Get(c.Data, key).String()
+	var dataMap map[string]interface{}
+	err := json.Unmarshal([]byte(c.Data), &dataMap)
+	if err != nil {
+		return ""
+	}
+
+	// Simple JSON path parsing for basic cases, can be extended for complex paths
+	keys := strings.Split(key, ".")
+	var current interface{} = dataMap
+	for _, k := range keys {
+		if m, ok := current.(map[string]interface{}); ok {
+			current = m[k]
+		} else {
+			return ""
+		}
+	}
+
+	return fmt.Sprintf("%v", current)
 }
 
 // HasMore 是否还有更多数据，仅分页且获取总数时返回
@@ -356,7 +458,10 @@ func (c *QimenResponse) HasMore() bool {
 // getSortedParams 获取排序后的请求参数
 func (c *QimenRequest) getSortedParams() ([]byte, error) {
 	// 提取所有键
-	mapParams := c.Params.(map[string]interface{})
+	mapParams, ok := c.Params.(map[string]interface{})
+	if !ok {
+		return nil, errors.New("params is not a map[string]interface{}")
+	}
 	keys := make([]string, 0, len(mapParams))
 	for k := range mapParams {
 		keys = append(keys, k)
@@ -388,8 +493,10 @@ type QimenContext struct {
 // Next 奇门上下文跳转
 func (c *QimenContext) Next() {
 	c.no += 1
-	nextFunc := *c.middlewares[c.no]
-	nextFunc(c)
+	if c.no < len(c.middlewares) {
+		nextFunc := *c.middlewares[c.no]
+		nextFunc(c)
+	}
 }
 
 // Use 添加奇门中间件
@@ -441,7 +548,7 @@ func (c *QimenClient) getSign(timestamp string, dataWrapper []byte, pager *Pager
 		connString += k + value
 	}
 	connString += c.Config.QimenAppSecret
-	sign := strings.ToUpper(gwdtUtils.MD5(connString))
+	sign := strings.ToUpper(md5Sum(connString))
 	params["sign"] = sign
 
 	return sign, wdtSign, params, nil
@@ -456,10 +563,10 @@ func (c *QimenClient) getWdtSign(datetime string, dataWrapper []byte, pager *Pag
 	params := map[string]string{
 		"method":           method,
 		"datetime":         datetime,
-		"params":           string(dataWrapper),
 		"wdt3_customer_id": c.Config.Sid,
 		"wdt_salt":         wdtSalt,
 		"wdt_appkey":       c.Config.WdtAppKey,
+		"params":           string(dataWrapper),
 	}
 	if pager != nil {
 		if pager.CalcTotal {
@@ -480,33 +587,32 @@ func (c *QimenClient) getWdtSign(datetime string, dataWrapper []byte, pager *Pag
 		connString += k + params[k]
 	}
 	connString += wdtSecret
-	return gwdtUtils.MD5(connString), nil
+	return md5Sum(connString), nil
 }
 
 // Call 执行奇门接口请求
 func (c *QimenClient) Call(request *QimenRequest) *QimenResponse {
-	rq := c.rq
-	tryMiddlewares := append(c.middlewares, &rq)
 	ctx := QimenContext{
 		Request:     request,
 		Response:    nil,
 		Client:      c,
-		middlewares: tryMiddlewares,
+		middlewares: c.middlewares,
 		no:          0,
 	}
-	nextFunc := *tryMiddlewares[0]
-	nextFunc(&ctx)
+	if len(c.middlewares) > 0 {
+		nextFunc := *c.middlewares[0]
+		nextFunc(&ctx)
+	}
 	return ctx.Response
 }
 
 // CallWithoutMiddleware 执行奇门接口请求，不执行中间件
 func (c *QimenClient) CallWithoutMiddleware(request *QimenRequest) *QimenResponse {
-	rq := c.rq
 	ctx := QimenContext{
 		Request:  request,
 		Response: nil,
 	}
-	rq(&ctx)
+	c.rq(&ctx)
 	return ctx.Response
 }
 
@@ -519,32 +625,19 @@ func (c *QimenClient) rq(ctx *QimenContext) {
 		DateTime: time.Now().Format("2006-01-02 15:04:05"),
 	}
 	var err error
-	var dataWrapper []byte
-	if request.Params == nil {
-		dataWrapper = []byte("{}")
-	} else {
-		if reflect.TypeOf(request.Params) == reflect.TypeOf(map[string]interface{}{}) {
-			dataWrapper, err = request.getSortedParams()
-			if err != nil {
-				res.Error = &QimenError{
-					Message:      "Failed to marshal params",
-					RequestError: err,
-				}
-				ctx.Response = &res
-				return
-			}
-		} else if reflect.TypeOf(request.Params) == reflect.TypeOf([]interface{}{}) {
-			dataWrapper, _ = json.Marshal(request.Params.([]interface{}))
-		} else {
+	dataWrapper := []byte("{}")
+	if request.Params != nil {
+		dataWrapper, err = json.Marshal(request.Params)
+		if err != nil {
 			res.Error = &QimenError{
 				Message:      "Failed to marshal params",
-				RequestError: errors.New("params type error"),
+				RequestError: err,
 			}
 			ctx.Response = &res
 			return
 		}
-
 	}
+
 	var params map[string]string
 	res.Sign, res.WdtSign, params, err = c.getSign(res.DateTime, dataWrapper, request.Pager, request.Method)
 	if err != nil {
@@ -555,23 +648,8 @@ func (c *QimenClient) rq(ctx *QimenContext) {
 		return
 	}
 
-	resp, err := grequests.Get(c.Config.QimenUrl, &grequests.RequestOptions{Params: params})
-	if err != nil {
-		res.Error = &QimenError{
-			RequestError: err,
-		}
-		ctx.Response = &res
-		return
-	} else if resp == nil {
-		res.Error = &QimenError{
-			RequestError: errors.New("request failed"),
-		}
-		ctx.Response = &res
-		return
-	}
-
-	var responseMap map[string]interface{}
-	err = json.Unmarshal([]byte(resp.String()), &responseMap)
+	// Build HTTP request
+	httpReq, err := http.NewRequest("GET", c.Config.QimenUrl, nil)
 	if err != nil {
 		res.Error = &QimenError{
 			RequestError: err,
@@ -580,26 +658,90 @@ func (c *QimenClient) rq(ctx *QimenContext) {
 		return
 	}
 
-	flag := gjson.Get(resp.String(), "response.flag").String()
+	q := httpReq.URL.Query()
+	for k, v := range params {
+		q.Add(k, v)
+	}
+	httpReq.URL.RawQuery = q.Encode()
+
+	client := &http.Client{Timeout: 30 * time.Second}
+	httpResp, err := client.Do(httpReq)
+	if err != nil {
+		res.Error = &QimenError{
+			RequestError: err,
+		}
+		ctx.Response = &res
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+
+	}(httpResp.Body)
+
+	body, err := ioutil.ReadAll(httpResp.Body)
+	if err != nil {
+		res.Error = &QimenError{
+			RequestError: err,
+		}
+		ctx.Response = &res
+		return
+	}
+
+	var rawResponse map[string]interface{}
+	err = json.Unmarshal(body, &rawResponse)
+	if err != nil {
+		res.Error = &QimenError{
+			RequestError: err,
+		}
+		ctx.Response = &res
+		return
+	}
+
+	responseMap, ok := rawResponse["response"].(map[string]interface{})
+	if !ok {
+		res.Error = &QimenError{
+			RequestError: errors.New("invalid response format: missing 'response' key"),
+		}
+		ctx.Response = &res
+		return
+	}
+
+	flag, _ := responseMap["flag"].(string)
 	if flag == "failure" {
 		res.Status = 1
 		res.Error = &QimenError{
 			Flag:         flag,
-			RequestId:    gjson.Get(resp.String(), "response.request_id").String(),
-			Code:         gjson.Get(resp.String(), "response.code").String(),
-			Message:      gjson.Get(resp.String(), "response.message").String(),
-			SubCode:      gjson.Get(resp.String(), "response.sub_code").String(),
-			SubMsg:       gjson.Get(resp.String(), "response.sub_message").String(),
+			RequestId:    fmt.Sprintf("%v", responseMap["request_id"]),
+			Code:         fmt.Sprintf("%v", responseMap["code"]),
+			Message:      fmt.Sprintf("%v", responseMap["message"]),
+			SubCode:      fmt.Sprintf("%v", responseMap["sub_code"]),
+			SubMsg:       fmt.Sprintf("%v", responseMap["sub_message"]),
 			RequestError: nil,
 		}
 		ctx.Response = &res
 		return
 	}
 	res.Status = 0
-	res.Data = gjson.Get(resp.String(), "response.data").String()
-	if request.Pager != nil {
-		if request.Pager.CalcTotal {
-			res.TotalCount = gjson.Get(res.Data, "total_count").Int()
+	dataVal, ok := responseMap["data"].(interface{})
+	if ok {
+		dataBytes, _ := json.Marshal(dataVal)
+		res.Data = string(dataBytes)
+	}
+
+	if request.Pager != nil && request.Pager.CalcTotal {
+		var dataMap map[string]interface{}
+		err = json.Unmarshal([]byte(res.Data), &dataMap)
+		if err != nil {
+			res.Error = &QimenError{
+				RequestError: err,
+			}
+			ctx.Response = &res
+			return
+		}
+		if totalCountVal, ok := dataMap["total_count"].(float64); ok {
+			res.TotalCount = int64(totalCountVal)
+		} else {
+			res.TotalCount = 0
 		}
 	} else {
 		res.TotalCount = 0
